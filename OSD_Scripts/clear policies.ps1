ddb2d432c8adc8c7dfb06f0fa4082fd3a7962113
Heiss Kasper <Kasper.Heiss@danfoss.com>
2018-03-23 12:29:08 +0100

    Fixed an issue, where the script was not trying to delete in the registry, but on the locale drive instead.

--- OSD_Scripts/clear policies.ps1
+++ OSD_Scripts/clear policies.ps1
@@ -1,4 +1,5 @@
 ﻿    New-PSDrive -PSProvider Registry -Name Users -Root HKEY_USERS
+    Set-Location Users:
      (Get-ChildItem "Users:\").PSPath | foreach { if($_){
             #write-host "Deleting " $_
             $policypath = $_ + "\software\policies"
@@ -12,10 +13,10 @@
     $Folder = ("HKLM:\SOFTWARE\Policies\")
 
     #Write-Host "starting to delete predefined machine policies"
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Hewlett-Packard" /f
-    reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\HP" /f
+    #reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft" /f
+    #reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Google" /f
+    #reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Hewlett-Packard" /f
+    #reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\HP" /f
     
     #Write-Host "starting to delete machine Policies"
     (Get-ChildItem $Folder).PSPath | foreach { if($_){
